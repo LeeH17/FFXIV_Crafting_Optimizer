987bd503973a18a6967096aac59f0575305f2812
MorganLLee79 <MorganLLee79@gmail.com>
2021-05-04 20:54:40 -0400

    Copied over IDDFS example from wikipedia

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,38 @@ fn main() {
     //say(message.as_bytes(), width, &mut writer).unwrap();
     println!("{}",message);
 }
+
+
+
+
+/*
+
+
+IDFFS example from wikipedia
+https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search
+
+function IDDFS(root) is
+    for depth from 0 to ∞ do
+        found, remaining ← DLS(root, depth)
+        if found ≠ null then
+            return found
+        else if not remaining then
+            return null
+
+function DLS(node, depth) is
+    if depth = 0 then
+        if node is a goal then
+            return (node, true)
+        else
+            return (null, true)    (Not found, but may have children)
+
+    else if depth > 0 then
+        any_remaining ← false
+        foreach child of node do
+            found, remaining ← DLS(child, depth−1)
+            if found ≠ null then
+                return (found, true)   
+            if remaining then
+                any_remaining ← true    (At least one node found at depth, let IDDFS deepen)
+        return (null, any_remaining)
+*/
